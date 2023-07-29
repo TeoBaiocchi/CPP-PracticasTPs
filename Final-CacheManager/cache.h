529110f5f153cdc2a4ba9695e7f89dbda35e4819
Teo <teobaiocchiensinck@gmail.com>
2023-07-29 00:47:06 -0300

    progreso. Abandono para estudiar rabasedas

--- Final-CacheManager/cache.h
+++ Final-CacheManager/cache.h
@@ -9,7 +9,8 @@ using namespace std;
 template <class T>
 class CacheManager{
     int capacity;
-    map<string, pair <T , int>> cache_data; // <Clave , <Obj , Indice de Uso>> El indice de uso se usa para obtener el LRU
+    int indice = 1;
+    map<string, pair<T , int>> cache_data; // <Clave , <Obj , Indice de Uso>> El indice de uso se usa para obtener el LRU
     string cacheFileName;
     bool write_file (string, T);
     void clear_file();
@@ -22,20 +23,101 @@ class CacheManager{
     T get(string key);
 
 // Agregar todas las funciones necesarias
+    void show_cache();
+    string getKeyLRU();
 };
 
 
-//NT: Sobre uso de templates, esencialmente es lo que permite que una función acepte tipos de forma dinámica. Es decir, un objeto T en vez de un tipo particular.
+
+//Constructor - Destructor ----->
 template <class T>
 CacheManager<T>::CacheManager(int cap, string cacheFileName){
     this->capacity = cap;
+
+    //NT: Agregué estas dos lineas ->
     this->cacheFileName = cacheFileName;
     this->clear_file();
 }
 
 template <class T>
 CacheManager<T>::~CacheManager(){}
+//<-----
 
+
+
+// Print ----->
+template <class T>
+void CacheManager<T>::show_cache(){
+    cout << "---------------" << endl << "Imprimiendo contenido de cache" << endl << "{";
+    for(const auto &x: cache_data){
+            cout << endl << " [ID "<< x.first << " - Indice Uso: " << x.second.second <<"]";
+            cout << "-> "; x.second.first.print();
+            //NT: CUIDADO! Esto asume que el tipo T tiene un metodo print. No se como imprimirlo genericamente sino...
+    }
+    cout << "}" << endl;
+}
+// <-----
+
+
+
+ //INSERT ----->
+template <class T>
+void CacheManager<T>::insert(string key, T obj){
+
+   if(cache_data.find(key) != cache_data.end()){
+        //Si se encuentra el elemento que se esta tratando de insertar se trata de una actualización.
+        cache_data[key] = make_pair(obj, indice++);
+        //no entiendo por qué da error
+        //return;
+   }
+
+
+   if(cache_data.size() >= capacity){
+        //En caso de que la capacidad sea alcanzada, borramos el LRU antes de realizar la inserción.
+        string keyLRU = getKeyLRU();
+        cout << "borrando a " << keyLRU << endl;
+        cache_data.erase(cache_data.find(keyLRU));
+   }
+    cout << "insertando a " << key << endl;
+    cache_data.insert(make_pair(key, make_pair(obj, indice++)));
+    write_file(key, obj);
+}
+//<-----
+
+
+//GET/Find by key ----->
+template <class T>
+T CacheManager<T>::get(string key){
+    auto it = cache_data.find(key);
+    if(it != cache_data.end()){ //Es decir, si se encuentra el elemento antes del fin del map
+        //Al encontrarlo también actualizamos su valor de acceso, como indica el ejemplo.
+        cache_data[key] = make_pair(cache_data[key].first, ++indice);
+        return cache_data[key];
+    }
+    return NULL; //TO DO: aca es donde al no encontrarse debería irse a memoria a buscarlo y devolver error si no está
+}
+// <-----
+
+
+template <class T>
+string CacheManager<T>::getKeyLRU(){
+    int menor = indice;
+    string keyActual;
+    for(const auto &x: cache_data){
+            if(x.second.second < menor){
+                menor = x.second.second; //indice de uso
+                keyActual = x.first;          //key
+            }
+        }
+    return keyActual;
+}
+
+
+
+
+//Manejo de archivos ----->
+//Incompleto, debido a mi escaso entendimiento y falta de atención. Esto es apenas
+//un esqueleto de lo que hay que hacer. Falta investigar toda la lectura y corroborar que esto siquiera esté bien
 template <class T>
 bool CacheManager<T>::write_file(string key, T obj){
       ofstream archivo(this->cacheFileName, ios::app);
@@ -50,18 +132,3 @@ void CacheManager<T>::clear_file(){
       archivo.close();
       return;
  }
-
-
- //INSERT
-template <class T>
-void CacheManager<T>::insert(string key, T obj){
-   this->cache_data.insert(make_pair(key, make_pair(obj, 0)));
-   //TODO algoritmo que reemplace
-   this->write_file(key, obj);
-}
-
-//GET / Find by key
-template <class T>
-T CacheManager<T>::get(string key){
-    return this->cache_data.find(key);
-}
--- Final-CacheManager/main.cpp
+++ Final-CacheManager/main.cpp
@@ -10,8 +10,9 @@ class Student{
         static const string class_name; //Cada clase tiene un static llamado : class_name
         Student(int _key, int _value, const char * _data ):id(_key), value( _value), data(_data){} //NT: Puede ser obvio, pero el uso de ":" es una forma de "abreviar" la declaración del constructor. Indica que dados esos parametros, correspodnen de esa manera a los valores puestos de la clase student.
 
-    void print(){
+    void print() const{
         cout << "Student Object : " << id << " , " << value << " , " << data << endl;
+        //Const es necesario para denotar que esto no va a cambiar el objeto y que pueda llamarlo entonces desde show_cache
     };
 
     ~Student(){};
@@ -25,18 +26,17 @@ int main(){
     CacheManager<Student> my_cache(3, cacheFileName); //NT: El agregado del segundo parametro fue decision mia y no venia con el enunciado
 
     my_cache.insert("0", Student(0, 22, "student1"));
-//    my_cache.insert("1", Student(1, 23, "student2"));
-//    my_cache.insert("2", Student(2, 25, "student3"));
-//    my_cache.insert("3", Student(3, 29, "student4"));
-//    my_cache.show_cache();
+    my_cache.insert("1", Student(1, 23, "student2"));
+    my_cache.insert("2", Student(2, 25, "student3"));
+    my_cache.insert("3", Student(3, 29, "student4"));
+    my_cache.show_cache();
 
     cout << "---------------------- UPDATE -----------------------" << endl;
     my_cache.insert("2", Student(22, 222, "EZE"));
-//    my_cache.show_cache();
+    my_cache.show_cache();
 
     cout << " <<<<<< <<<<<<<<<<<< <<<<<<<<<<<< <<<<<<<<<<< <<<<<<<<<<<< <<<<" << endl;
 //    Student return_obj = my_cache.get(" 0 "); // not in cache but in filesystem
-    //Al tener 4 valores, uno debe ser reemplazado por tener el limite de 3 en cache... y "0" es el LRU.
     cout << " <<<<<< <<<<<<<<<<<< <<<<<<<<<<<< <<<<<<<<<<< <<<<<<<<<<<< <<<<" << endl;
 
 //    my_cache.insert("2", Student(2, 25, "EZE"));
